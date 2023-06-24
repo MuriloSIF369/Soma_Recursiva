52646dac92942d71a6d14ce7a15104bc7f0bf319
Admin <Admin@MURILOPC>
2023-06-24 20:20:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Function math = new Function();
+Console.Write("Número da soma: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+double result = math.Sum(number);
+
+Console.Write($"Número somado: {result}");
+public class Function
+{
+    public double Sum(double number)
+    {
+        if (number == 1)
+            return number;
+
+        return (2 * number - 1) + Sum(number - 1);
+    }
+}
